005af061771a263a271692274e587a09245f4ee9
jalejoserna123 <josea.serna@udea.edu.co>
2018-03-02 10:31:04 -0500

    ejercicio 8

--- Ejercicio8/main.cpp
+++ Ejercicio8/main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "h" << endl;
+    int num, factorial=1;
+    cout << "Ingrese numero para calcular factorial" << endl;
+    cin>>num;
+    while(num>0){
+        factorial=factorial*num;
+        num=num-1;
+    }
+    cout<<"El factorial del numero es igual a:"<<factorial<<endl;
     return 0;
 }
